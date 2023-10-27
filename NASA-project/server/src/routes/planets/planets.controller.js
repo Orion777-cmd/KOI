bdf36d43916d6fde65a583763d14622b267e2257
Orion777-cmd <abiy.biru78@gmail.com>
2023-10-27 22:44:17 +0300

    added the file structure of the server folder/ mvc model

---
+++ NASA-project/server/src/routes/planets/planets.controller.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const planets = require('../../models/planets.model');
+
+const getAllPlanets = (req, res) => {
+    return res.status(200).json(planets)
+}
+
+module.exports = {
+    getAllPlanets
+}
