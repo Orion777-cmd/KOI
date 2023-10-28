f639adfdb4a337c2167ca7c0e98292242c4fc0df
Orion777-cmd <abiy.biru78@gmail.com>
2023-10-28 16:57:53 +0300

    installed jest and supertest and added some test functions for the /launches end point

--- NASA-project/client/src/hooks/requests.js
+++ NASA-project/client/src/hooks/requests.js
@@ -32,8 +32,7 @@ async function httpSubmitLaunch(launch) {
 }
 
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  
   try{
     return await fetch(`${API_URL}/launches/${id}`, {
       method: 'delete',
---
+++ NASA-project/server/src/routes/launches/launches.test.js
@@ -0,0 +1,69 @@
+const request = require('supertest');
+const app = require('../../app');
+describe('Test GET /launches ', () => {
+    test('it should respoond with 200 success', async () => {
+        const response = await request(app).get('/launches')
+        
+        expect(response.statusCode).toBe(200);
+        
+    })
+})
+
+
+describe('Test POST /launches', () => {
+    const completeLaunchData = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-62 f',
+        launchDate: 'January 4, 2028',
+    };
+
+    const launchDataWithoutDate = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-62 f',
+    };
+
+    test('it should respond with 201 created', async () => {
+        const response = await request(app).post('/launches').send(completeLaunchData)
+        .expect('Content-Type', /json/)
+        .expect(201);
+
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+        const responseDate = new Date(response.body.launchDate).valueOf();
+
+        expect(responseDate).toBe(requestDate);
+
+        expect(response.body).toMatchObject(launchDataWithoutDate);
+    })
+
+    test('it should catch missing required properties', async () => {
+        const response = await request(app).post('/launches').send({
+            mission: 'USS Enterprise',
+            rocket: 'NCC 1701-D',
+            launchDate: 'January 4, 2028',
+        
+        
+        });
+        expect(response.statusCode).toBe(400);
+    })
+
+    test('it should catch invalid dates', async () => {
+        const response = await request(app).post('/launches').send({
+            mission: 'USS Enterprise',
+            rocket: 'NCC 1701-D',
+            target: 'Kepler-62 f',
+            launchDate: 'abiy biru',
+        
+        
+        });
+        expect(response.statusCode).toBe(400);
+    })
+})
+
+describe('Test DELETE /launches', () => {
+    test('it should respond with 200 success',async () => {
+        const response = await request(app).delete('/launches/:100');
+        expect(response.statusCode).toBe(200);
+    })
+})
